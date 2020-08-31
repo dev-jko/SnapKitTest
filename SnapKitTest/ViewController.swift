f8acd7c1fa3097b78e2213e8dffaa0ca24107d12
nadarm <ccc7497@gmail.com>
2020-08-31 23:00:17 +0900

    snapkit

--- SnapKitTest/ViewController.swift
+++ SnapKitTest/ViewController.swift
@@ -11,7 +11,13 @@ import SnapKit
 
 class ViewController: UIViewController {
 
-    private let view1 = UIView()
+    private let views: [UIView] = {
+        var arr = [UIView]()
+        (0...30).forEach { _ in arr.append(UIView()) }
+        return arr
+    } ()
+    
+    private let colors: [UIColor] = [.green, .cyan, .red, .yellow, .black , .brown]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,19 +26,38 @@ class ViewController: UIViewController {
     }
 
     private func setUpLayout() {
-        view.addSubview(view1)
+        views
+            .forEach { view.addSubview($0) }
         
-        view1.snp.makeConstraints { maker in
-            maker.top.equalTo(view)
-            maker.bottom.equalTo(view)
-            maker.leading.equalTo(view)
-            maker.trailing.equalTo(view)
+        views[0].snp.makeConstraints { make in
+            make.top.equalTo(view)
+            make.bottom.equalTo(view)
+            make.leading.equalTo(view)
+            make.trailing.equalTo(view)
+        }
+        
+        views[0].snp.remakeConstraints { make in
+            make.edges.equalTo(view).inset(UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20))
+        }
+        
+        views[1].snp.makeConstraints { make in
+//            make.centerX.equalTo(views[0])
+            make.centerX.lessThanOrEqualTo(views[0].snp.trailing)
+            make.width.equalTo(200)
+            make.height.equalTo(100)
+        }
+        
+        views[2].snp.makeConstraints { make in
+            make.width.greaterThanOrEqualTo(200)
+            make.width.lessThanOrEqualTo(400)
+            make.height.equalTo(100)
+            make.trailing.lessThanOrEqualTo(-10)
         }
     }
 
     private func bindStyles() {
         view.backgroundColor = .white
-        view1.backgroundColor = .green
+        zip(views, colors).forEach { view, color in view.backgroundColor = color }
     }
 }
 
